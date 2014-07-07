ac4ff8ce10999c986e6cf9be1f70ca5d67010575
TiagoLr <prog4mr@gmail.com>
2014-07-07 04:00:16 +0100

    rename

--- src/com/onatbas/akaloader/utils/LoaderGroup.hx
+++ src/com/onatbas/akaloader/utils/LoaderGroup.hx
@@ -2,24 +2,24 @@ package com.onatbas.akaloader.utils;
 import com.onatbas.akaloader.AkaLoader;
 
 /**
- * Couple of utils to manage groups of LoaderManagers.
+ * Couple of utils to manage a group of LoaderManagers.
  * @author TiagoLr
  */
 class LoaderGroup {
 
-	static var groups:Map<String,AkaLoader> = new Map<String,AkaLoader>();
+	static var managers:Map<String,AkaLoader> = new Map<String,AkaLoader>();
 	
 	public static function registerManager(manager:AkaLoader, id:String) {
-		groups.set(id, group);
+		managers.set(id, group);
 	}
 	
 	public static function getManager(id:String):AkaLoader {
-		groups[id];
+		managers[id];
 	}
 	
 	public static function unregisterManager(id:String) {
-		if (groups.exists(id)) {
-			groups.remove(id);
+		if (managers.exists(id)) {
+			managers.remove(id);
 		}
 	}
 	
